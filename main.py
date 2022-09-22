6aceab3de382a2c54671da6c3921e8ec8b3a2799
jrodr117 <jrodr113@msudenver.edu>
2022-09-22 15:47:05 -0600

    added dictionary that stores calculator operations and used a for loop to print out the keys

--- main.py
+++ main.py
@@ -12,3 +12,17 @@ def multiplication(number1, number2):
 
 def division(number1, number2):
     return number1 / number2
+
+
+calculator_dictionary = {
+    "+": addition,
+    "-": subtraction,
+    "*": multiplication,
+    "/": division,
+}
+
+num1 = int(input("First number:"))
+num2 = int(input("Second number:"))
+
+for key in calculator_dictionary:
+    print(key)
